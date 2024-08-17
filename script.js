9e691ef0f1965608fe2553c364b4ed53fc798945
rip-the-slit <171191896+rip-the-slit@users.noreply.github.com>
2024-08-17 12:27:01 -0430

    Create book object constructor

---
+++ script.js
@@ -0,0 +1,11 @@
+const myLibrary = [];
+
+function Book(title, author, genre, numberOfPages) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.numberOfPages = numberOfPages;
+}
+
+function addBookToLibrary() {
+}

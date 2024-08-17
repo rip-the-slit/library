155da822db0f99aea36d08d598006bf9520e3ba4
rip-the-slit <171191896+rip-the-slit@users.noreply.github.com>
2024-08-17 17:22:38 -0430

    Create function to display library

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const myLibrary = [];
+const container = document.querySelector('.container')
 
 function Book(title, author, genre, numberOfPages) {
-    this.title = title;
-    this.author = author;
-    this.genre = genre;
-    this.numberOfPages = numberOfPages;
+    this['Title'] = title;
+    this['Author'] = author;
+    this['Genre'] = genre;
+    this['Number of Pages'] = numberOfPages;
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+function refreshLibrary() {
+    myLibrary.forEach((book, index) => {
+        const card = document.createElement('div')
+        card.classList = "card"
+        for (let property in book) {
+            const p = document.createElement('p')
+            const key = document.createElement('span')
+            const value = document.createElement('em')
+
+            key.classList = 'key'
+            key.textContent = property + ':'
+
+            value.classList = 'value'
+            value.textContent = book[property]
+
+            p.appendChild(key)
+            p.appendChild(value)
+            card.appendChild(p)
+        }
+        container.appendChild(card)
+    })
+}
